01ac811097054431ee70f95fea0c69800bab1be2
felipe stival <v0idpwn@gmail.com>
2019-07-06 16:50:30 -0300

    Initial commit

---
+++ lib/server.h
@@ -0,0 +1,22 @@
+class Response {
+    public:
+        Response (int statusA, char* payloadA);  
+        Response();
+        unsigned int status;
+        char payload[120];
+};
+
+class Serv { 
+    public:
+        int& counting, milisecondsPassed;
+        double& current;
+        Serv(int &countingA, int &milisecondsPassedA, double &currentA);
+        Response runCommand(unsigned int command);
+    private: 
+        Response getStatus();
+        Response getCount();
+        Response startCount();
+        Response pauseCount();
+        Response continueCount();
+        Response stopCount();
+};
---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+#include <Arduino.h>
+#include <Server.h>
+
+#define BUZZER_FET 30
+#define IGNITE_FET 31
+#define CURRENT_IN A0
+#define MAX_TIME 30000
+#define BAUD 9600
+#define READ_RATE 200
+
+// Initializing vars
+unsigned int READ_INTERVAL = 1000 / READ_RATE;
+unsigned long int t0 = 0;
+unsigned long int t = 0;
+double current = 0;
+bool counting = false;
+unsigned long int miliseconds_passed = 0;
+unsigned long int delta;
+Response r = new Response();
+Serv sv = new Serv();
+
+// Declaring functions
+/* Returns the current in the CURRENT_IN pin */
+double readCurrent(){
+  return analogRead(CURRENT_IN);
+}
+
+/* Encodes the response into a human readable string */
+void encodeResponse(char *buffer, Response r){
+  sprintf(buffer, "%d - %s", r.status, r.payload);
+}
+
+/* Starts the ignition process; */
+void ignite(){
+  digitalWrite(IGNITE_FET, HIGH);
+}
+
+/* Stops the ignition process */
+void unignite(){
+  digitalWrite(IGNITE_FET, LOW);
+}
+
+// Arduino functions
+
+void setup() {
+  Serial1.begin(BAUD);  
+  pinMode(BUZZER_FET, OUTPUT);
+  pinMode(IGNITE_FET, OUTPUT);
+}
+
+void loop() {
+  t = millis();
+  if(t > (t0 + READ_INTERVAL)){
+    current = readCurrent();
+    if(Serial.available() > 0){
+      r = sv.runCommand(Serial.read());
+      char buffer[64];
+      encodeResponse(buffer, r);
+      Serial1.write(buffer);
+    } 
+    if(counting){
+      delta = t - t0;
+      if(miliseconds_passed > MAX_TIME){
+        ignite();
+      }else{
+        miliseconds_passed += delta;
+      }
+    }
+    t0 = t;
+  }
+}
